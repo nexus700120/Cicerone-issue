95c69e18b4e80df25d795567ffe593bb598ea838
Vitaly Kirillov <vkirillov@swiftcom.uk>
2021-09-06 11:19:18 +0300

    Sync v7.0 sample

--- app/src/main/java/ru/cicerone/issue/Navigator.kt
+++ app/src/main/java/ru/cicerone/issue/Navigator.kt
@@ -1,9 +1,15 @@
 package ru.cicerone.issue
 
 import androidx.fragment.app.Fragment
+import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.androidx.AppNavigator
 
 class Navigator(
     containerId: Int,
     fragment: Fragment
-) : AppNavigator(fragment.requireActivity(), containerId, fragment.childFragmentManager)
+) : AppNavigator(fragment.requireActivity(), containerId, fragment.childFragmentManager) {
+
+    override fun applyCommands(commands: Array<out Command>) {
+        applyCommandsSync(commands)
+    }
+}
